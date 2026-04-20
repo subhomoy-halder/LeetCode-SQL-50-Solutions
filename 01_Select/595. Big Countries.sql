07d3151bc959a4bfe43407c68ce4d883e88072ab
Subhomoy Halder <subhomoy7halder@gmail.com>
2026-04-20 10:52:16 +0530

    Update 595. Big Countries.sql

--- 01_Select/595. Big Countries.sql
+++ 01_Select/595. Big Countries.sql
@@ -7,15 +7,6 @@ The requirement is to filter a dataset based on two independent thresholds:
 geographic size (area) or demographic density (population). We use the OR 
 logical operator to return a record if it satisfies at least one of these 
 discrete business rules.
-
-ARCHITECTURAL NOTE: 
-1. Predicate Evaluation: When using an 'OR' across two different columns, 
-   database engines often perform an "Index Merge" if separate indexes exist 
-   on both 'area' and 'population'. 
-2. Performance Trade-off: In extremely large datasets, some architects 
-   prefer using a 'UNION' of two separate SELECT statements to ensure 
-   individual index usage, though for a standard table of this scale, a 
-   single 'OR' is the most readable and efficient approach.
 */
 
 SELECT 
